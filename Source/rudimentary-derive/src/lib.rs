cea981843bcafae69515e17016ad98aa01b654f3
woksin <sindrewilting@gmail.com>
2021-03-10 14:53:18 +0100

    More fine-grained Concepts

--- Source/rudimentary-derive/src/lib.rs
+++ Source/rudimentary-derive/src/lib.rs
@@ -1,10 +1,8 @@
 // Taken from https://github.com/PrismaPhonic/domain_patterns/master/domain_derive/src/lib.rs
 
-use quote::quote;
-use syn::parse_macro_input;
-
 mod concept_as;
-
+use syn::parse_macro_input;
+use quote::quote;
 use proc_macro::TokenStream;
 use syn::DeriveInput;
 
@@ -40,6 +38,56 @@ pub fn derive_concept_setup(input: TokenStream) -> TokenStream {
             }
         }
 
+        impl std::convert::From<#value_type_name> for #concept_name {
+
+            fn from(value: #value_type_name) -> Self {
+                #concept_name {value}
+            }
+        }
+        impl rudimentary::ConceptBase<#value_type_name> for #concept_name {
+            fn value(&self) -> #value_type_name {
+                self.value
+            }
+        }
+        impl rudimentary::ConceptAs<#value_type_name> for #concept_name {
+            fn new(value: #value_type_name) -> Self {
+                Self::from(value)
+            }
+        }
+    };
+
+    TokenStream::from(expanded)
+}
+#[proc_macro_derive(ValidatedConceptSetup)]
+pub fn derive_validated_concept_setup(input: TokenStream) -> TokenStream {
+    let input: DeriveInput = parse_macro_input!(input as DeriveInput);
+    let concept_name = &input.ident;
+
+    concept_as::precondition(&input).expect(FAILED_PRECONDITION);
+
+    let value_type_name = &concept_as::value_type_name(&input.data).unwrap();
+
+    let expanded = quote! {
+        impl std::fmt::Display for #concept_name {
+            fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+                write!(f, "{}", self.value)
+            }
+        }
+
+        impl std::cmp::PartialEq for #concept_name {
+            fn eq(&self, other: &Self) -> bool {
+                self.value == other.value
+            }
+        }
+
+        impl std::clone::Clone for #concept_name {
+            fn clone(&self) -> Self {
+                #concept_name {
+                    value: self.value.clone()
+                }
+            }
+        }
+
         impl std::convert::TryFrom<#value_type_name> for #concept_name {
             type Error = &'static str;
 
@@ -47,6 +95,12 @@ pub fn derive_concept_setup(input: TokenStream) -> TokenStream {
                 Self::validate(&value)?;
 
                 Ok(#concept_name {value})
+            }
+        }
+
+        impl rudimentary::ConceptBase<#value_type_name> for #concept_name {
+            fn value(&self) -> #value_type_name {
+                self.value
             }
         }
     };
--- Source/rudimentary/src/concept_as.rs
+++ Source/rudimentary/src/concept_as.rs
@@ -1,14 +1,8 @@
 // From https://github.com/PrismaPhonic/domain_patterns/blob/master/domain_derive/src/value_object.rs
 pub use std::fmt::Display;
-pub use std::convert::TryFrom;
+pub use std::convert::{From, TryFrom};
 
-pub trait ConceptAs<TValue>: Clone + PartialEq + TryFrom<TValue> + Display {
-
-    /// `validate` takes in incoming data used to construct the value object, and validates it against
-    /// given constraints.  An example would be if we had an `Email` struct that implements `ValueObject`.
-    /// The constraints we would check would ensure that the incoming data is a valid email address.
-    fn validate(value: &TValue) -> Result<(), &'static str>;
-
+pub trait ConceptBase<TValue> {
     /// `value` return a reference to the internal value held in the value object. This should be the only
     /// way that we access the internal data.  Mutation methods should always generate a new value object.
     /// Note: It's intentional that value returns an owned type.  This is necessary for enums, where we likely
@@ -17,4 +11,16 @@ pub trait ConceptAs<TValue>: Clone + PartialEq + TryFrom<TValue> + Display {
     /// value, not a ref (the string that was freshly created would be dropped at the end of value() if we try to pass
     /// back a ref of it).
     fn value(&self) -> TValue;
+}
+
+pub trait ConceptAs<TValue>: ConceptBase<TValue> + Clone + PartialEq + From<TValue> + Display{
+    fn new(value: TValue) -> Self;
+}
+
+pub trait ValidatedConceptAs<TValue>: ConceptBase<TValue> + Clone + PartialEq + TryFrom<TValue> + Display {
+
+    /// `validate` takes in incoming data used to construct the value object, and validates it against
+    /// given constraints.  An example would be if we had an `Email` struct that implements `ValueObject`.
+    /// The constraints we would check would ensure that the incoming data is a valid email address.
+    fn validate(value: &TValue) -> Result<(), &'static str>;
 }
